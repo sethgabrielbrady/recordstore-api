9b84fc9527a219ea6fbca88714f08f4849f36cf4
Seth Brady <sethgabrielbrady@gmail.com>
2020-02-10 06:20:58 -0500

    Add signup/login/and refresh features; Updates routes, initializers;

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
       resources :artists
     end
   end
+
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
